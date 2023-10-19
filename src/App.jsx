07bbdb9c8a3d567d4fb4c30506ef8797d19586a2
Luke Wheldale <lukewheldale@gmail.com>
2023-10-19 14:35:55 +1030

    playing around with props, state, passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,69 @@
 import "./App.css";
+import { useState } from "react";
 
-function MyButton() {
-	return <button> I&apos;m a button</button>;
+function MyButton({ count, onClick }) {
+	return (
+		<button onClick={onClick} style={{ backgroundColor: "green" }}>
+			Clicked {count} times
+		</button>
+	);
 }
 
-function App() {
+export default function App() {
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+		setCount(count + 1);
+	}
 	return (
 		<>
 			<h1>Welcome to my app</h1>
-			<MyButton />
+			<MyButton count={count} onClick={handleClick} />
+			<MyButton count={count} onClick={handleClick} />
 		</>
 	);
 }
 
-export default App;
+// const user = {
+// 	name: "Hedy Lamarr",
+// 	imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+// 	imageSize: 90,
+// };
+
+// export default function Profile() {
+// 	return (
+// 		<>
+// 			<h1>{user.name}</h1>
+// 			<img
+// 				className="avatar"
+// 				src={user.imageUrl}
+// 				alt={"Photo of " + user.name}
+// 				style={{
+// 					width: user.imageSize,
+// 					height: user.imageSize,
+// 				}}
+// 			/>
+// 		</>
+// 	);
+// }
+
+// const products = [
+// 	{ title: "Cabbage", isFruit: false, id: 1 },
+// 	{ title: "Garlic", isFruit: false, id: 2 },
+// 	{ title: "Apple", isFruit: true, id: 3 },
+// ];
+
+// export default function ShoppingList() {
+// 	const listItems = products.map((product) => (
+// 		<li
+// 			key={product.id}
+// 			style={{
+// 				color: product.isFruit ? "blue" : "red",
+// 			}}
+// 		>
+// 			{product.title}
+// 		</li>
+// 	));
+
+// 	return <ul>{listItems}</ul>;
+// }
